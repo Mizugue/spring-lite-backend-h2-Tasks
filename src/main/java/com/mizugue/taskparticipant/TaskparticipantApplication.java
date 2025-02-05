07b386b099829e54f8520dc59a86452d1d6c90e3
Mzg. <jchallaksantos@gmail.com>
2025-02-05 08:39:26 -0300

    init

---
+++ src/main/java/com/mizugue/taskparticipant/TaskparticipantApplication.java
@@ -0,0 +1,13 @@
+package com.mizugue.taskparticipant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskparticipantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskparticipantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mizugue/taskparticipant/TaskparticipantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mizugue.taskparticipant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskparticipantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
